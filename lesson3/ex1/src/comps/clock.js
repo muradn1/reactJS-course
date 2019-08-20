27dc7f92dea3b16184abad1214242cb1c75d3dbf
Hackeru <Hackeru@Hackeru2.local>
2019-08-20 09:12:19 +0300

    update

---
+++ lesson3/ex1/src/comps/clock.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Clock extends Component {
+    render() {
+        return (
+            <div>
+                Clock work
+            </div>
+        )
+    }
+}
+
+export default Clock;
